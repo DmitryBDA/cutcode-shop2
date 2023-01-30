b48b30b4dc9ff239593d90b58a55484d3ba7a17e
Belousov Dmitry <da_belousov@list.ru>
2023-01-31 01:42:41 +0800

    stage 2 done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome123');
+    logger()->channel('telegram')->debug('hello');
+    return view('welcome');
 });
